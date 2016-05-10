0a5383cabd804e37a4987bf3b8ec496602651a88
Yuki Kodama <endflow.net@gmail.com>
2016-05-10 10:45:29 +0900

    Bugfix and add test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default function mergeable(reducer) {
   reducer.original = reducer;
-  reducer.merge = function merge() {
+  reducer.merge = function merge(extra) {
     const original = this.original;
     return function wrapper(state, action) {
       const newState = extra(state, action);
--- test/index.js
+++ test/index.js
@@ -13,3 +13,51 @@ describe('mergeable', () => {
     assert(typeof reducer.merge === 'function');
   });
 });
+
+describe('mergeable.merge', () => {
+  const initial = { foo: 0, bar: false };
+  const extra = (state, action) => {
+    switch (action.type) {
+    case 'BAR':
+      return { ...state, bar: !state.bar };
+    }
+    return state;
+  };
+  let shared;
+
+  beforeEach(() => {
+    shared = mergeable((state = initial, action) => {
+      switch (action.type) {
+      case 'FOO':
+        return { ...state, foo: state.foo + 1 };
+      }
+      return state;
+    });
+  });
+
+  it("returns merged reducer", () => {
+    const merged = shared.merge(extra);
+    assert(merged !== shared);
+    assert(merged !== extra);
+
+    const s0 = merged(undefined, { type: '@@INIT' });
+    assert.deepEqual(s0, { foo: 0, bar: false });
+
+    const s1 = merged(s0, { type: 'FOO' });
+    assert.deepEqual(s1, { foo: 1, bar: false });
+
+    const s2 = merged(s1, { type: 'BAR' });
+    assert.deepEqual(s2, { foo: 1, bar: true });
+  });
+
+  it("can be used without merging", () => {
+    const s0 = shared(undefined, { type: '@@INIT' });
+    assert.deepEqual(s0, { foo: 0, bar: false });
+
+    const s1 = shared(s0, { type: 'FOO' });
+    assert.deepEqual(s1, { foo: 1, bar: false });
+
+    const s2 = shared(s1, { type: 'BAR' });
+    assert.deepEqual(s2, { foo: 1, bar: false });
+  });
+});
